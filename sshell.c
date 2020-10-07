2709ad94d01c047c0b471a2779887b3d2be09b5f
okhan98 <okhan98@aol.com>
2020-10-06 18:32:20 -0700

    phase 2 partially complete

--- sshell.c
+++ sshell.c
@@ -9,17 +9,18 @@
 
 struct Input{
     char cmd[CMDLINE_MAX];
-    char args[ARGS_MAX];
+    char *args[ARGS_MAX];
     char tokens[TOKEN_MAX];
 };
 
 int main(void)
 {
-        struct Input input;
-
-        while (1) {
+    struct Input input;
+    while (1) {
                 char *nl;
                 int retval;
+
+                pid_t pid;
 
                 /* Print prompt */
                 printf("sshell$ ");
@@ -44,11 +45,50 @@ int main(void)
                         fprintf(stderr, "Bye...\n");
                         break;
                 }
+                char message[CMDLINE_MAX];
+                strcpy(message, input.cmd);
+                printf("message: %s \n", message);
 
+                // String Tokenizer
+                char *token = strtok(input.cmd, " ");
+                int counter = 0;
+                while(token != NULL)
+                {
+                    if(counter == 0)
+                    {
+                        input.cmd[0] = *token;
+                    }
+                    if(counter > 0)
+                    {
+                        for(int i = 0; i < ARGS_MAX; i++)
+                            input.args[i] = token;
+                    }
+                    token = strtok(NULL, " ");
+                    counter = counter + 1;
+                }
                 /* Regular command */
-                retval = system(input.cmd);
-                fprintf(stdout, "Return status value for '%s': %d\n",
-                        input.cmd, retval);
+                pid = fork();
+
+                if(pid == 0){
+                    // Child
+                    retval = execvp(input.cmd, input.args);
+                    perror("execvp");
+                    exit(1);
+                }
+                else if(pid > 0) {
+
+                    // Parent
+                    int status;
+                    waitpid(pid, &status, 0);
+                    printf("+ completed '%s' [%d] \n", message, WEXITSTATUS(status));
+
+                }
+                else{
+                        perror("fork");
+                        exit(1);
+                    }
+             //   fprintf(stdout, "Return status value for '%s': %d\n", input.cmd, retval);
+
         }
 
         return EXIT_SUCCESS;
