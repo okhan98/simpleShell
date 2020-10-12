2792872892d712d4da11b7008ec99ccc7b1fb943
Ethan Turner <17074013+ethanaturner@users.noreply.github.com>
2020-10-11 18:08:46 -0700

    Improve input parsing and introduce file redirection.

--- sshell.c
+++ sshell.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 #define CMDLINE_MAX 512
@@ -11,12 +12,82 @@ struct Input{
     char cmd[CMDLINE_MAX];
     char *args[ARGS_MAX];
     char tokens[TOKEN_MAX];
+    char file[TOKEN_MAX];
+};
+
+void parseInput(struct Input *input) {
+    /* String Tokenizer */
+    char *token = strtok(input->cmd, " ");
+    int counter = 0;
+    while(token != NULL)
+    {
+        if (counter == 0) {
+            input->cmd[0] = *token;
+            input->args[0] = token;
+        }
+        if (counter > 0) {
+            input->args[counter] = token;
+        }
+        token = strtok(NULL, " ");
+        counter++;
+    }
+    input->args[counter] = NULL; // Sets last argument to NULL to prevent whitespace/empty argument issues
+};
+
+int checkRedirect(struct Input *input) {
+    int willRedirect = 0;
+    for (int i = 0; input->args[i] != NULL; i++) {
+            for(size_t k = 0; k < strlen(input->args[i]); k++) {
+                if (input->args[i][k] == '>') {
+                    if (k !=0 && k != strlen(input->args[i])-1) {
+                        printf("Found redirection character in middle of token at position: %lu \n", k);
+                    }
+                }
+            }
+
+            size_t argsLen = strlen(*input->args);
+            size_t tokenLen = strlen(input->args[i]);
+
+            /* meta-character is at the beginning of the token */
+            if (input->args[i][0] == '>') {
+                    if (input->args[i][1] != '\0') {
+                            /* File name does not have a whitespace next to it */
+                            char *name = input->args[i] + 1;
+                            char fileName[strlen(name) + 1];
+                            strcpy(fileName, name);
+                            strcpy(input->file, fileName);
+                            /* Shift the char array to remove the file name argument */
+                            for (size_t j = i; j < argsLen; j++) {
+                                    input->args[j] = input->args[j+1];
+                                    input->args[strlen(*input->args)] = NULL;
+                            }
+                    } else if (input->args[i][1] == '\0') {
+                            /* File name has a whitespace next to it */
+                            strcpy(input->file, input->args[i+1]);
+                            for (size_t j = i; j < argsLen; j++) {
+                                input->args[j] = input->args[j+2];
+                                input->args[strlen(*input->args)] = NULL;
+                            }
+                    }
+                    willRedirect = 1;
+            } else if (input->args[i][tokenLen-1] == '>') {
+                    strcpy(input->file, input->args[i+1]); // File name should be the next argument
+                    input->args[i][tokenLen-1] = '\0'; // Removes the meta-character from the argument
+                    for (size_t j = i; j < argsLen; j++) {
+                            input->args[j+1] = input->args[j+2];
+                            input->args[strlen(*input->args)] = NULL;
+                    }
+                    willRedirect = 1;
+            }
+  }
+  return willRedirect;
 };
 
 int main(void)
 {
     struct Input input;
     while (1) {
+
                 char *nl;
                 int retval;
 
@@ -49,35 +120,33 @@ int main(void)
                 strcpy(message, input.cmd);
                 printf("message: %s \n", message);
 
-                // String Tokenizer
-                char *token = strtok(input.cmd, " ");
-                int counter = 0;
-                while(token != NULL)
-                {
-                    if(counter == 0)
-                    {
-                        input.cmd[0] = *token;
-                    }
-                    if(counter > 0)
-                    {
-                        for(int i = 0; i < ARGS_MAX; i++)
-                            input.args[i] = token;
-                    }
-                    token = strtok(NULL, " ");
-                    counter = counter + 1;
+                /* Send the input to be parsed for arguments */
+                parseInput(&input);
+                int willRedirect = checkRedirect(&input);
+
+                /* Remove this later, for debugging */
+                printf("[DEV]: list of arguments: \n");
+                for (int i = 0; input.args[i] != NULL; i++) {
+                    printf("%s \n", input.args[i]);
                 }
+
                 /* Regular command */
-                input.args[counter] = NULL; // Resets args
                 pid = fork();
 
                 if(pid == 0){
                     // Child
+                    int fd;
+                    if (willRedirect == 1) {
+                        fd = open(input.file, O_WRONLY | O_CREAT, 0644);
+                        dup2(fd, STDOUT_FILENO);
+                        close(fd);
+                    }
                     retval = execvp(input.cmd, input.args);
                     perror("execvp");
+
                     exit(1);
                 }
                 else if(pid > 0) {
-
                     // Parent
 
                     // Change directory
@@ -92,9 +161,9 @@ int main(void)
 
                 }
                 else{
-                        perror("fork");
-                        exit(1);
-                    }
+                    perror("fork");
+                    exit(1);
+                }
              //   fprintf(stdout, "Return status value for '%s': %d\n", input.cmd, retval);
 
         }
