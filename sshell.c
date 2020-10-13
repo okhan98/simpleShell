02928c589b9d4067ea11fb1a168f95efc0e8e1b6
Ethan Turner <17074013+ethanaturner@users.noreply.github.com>
2020-10-13 16:01:53 -0700

    Piping and output redirection improvements.

--- sshell.c
+++ sshell.c
@@ -13,28 +13,22 @@ struct Input{
     char *args[ARGS_MAX];
     char tokens[TOKEN_MAX];
     char file[TOKEN_MAX];
-};
-
-struct Pipe{
- char *command[CMDLINE_MAX];
+    int willRedirect;
 };
 
 void parsePipe(struct Input input[CMDLINE_MAX], char raw[CMDLINE_MAX], int *commandCount) {
     /* String Tokenizer */
-    char *nl;
     int counter = 0;
     char *token = strtok(raw, "|");
     while(token != NULL) {
-        /* Remove trailing newline from command line */
-        nl = strchr(token, '\n');
+        /* Remove trailing whitespace from command line */
         if(token[strlen(token)-1] == ' ') {
             token[strlen(token)-1] = '\0';
-
         }
+        /* Remove leading whitespace from command line */
         if(token[0] == ' '){
             token++;
         }
-
         struct Input temp;
         strcpy(temp.cmd, token);
         input[counter] = temp;
@@ -63,74 +57,82 @@ void parseInput(struct Input *input) {
     input->args[counter] = NULL; // Sets last argument to NULL to prevent whitespace/empty argument issues
 };
 
-int checkRedirect(struct Input *input) {
-    int willRedirect = 0;
-    for (int i = 0; input->args[i] != NULL; i++) {
-            for(size_t k = 0; k < strlen(input->args[i]); k++) {
-                if (input->args[i][k] == '>') {
-                    if (k !=0 && k != strlen(input->args[i])-1) {
-                        size_t length = strlen(input->args[i]);
-                        char currToken[length];
-                        strcpy(currToken, input->args[i]);
-                        char *newcmd = strtok(input->args[i], ">");
-                        input->args[0] = newcmd;
-                        newcmd = strtok(NULL, ">");
-                        strcpy(input->file, newcmd);
-                        willRedirect = 1;
-                        size_t argLen = strlen(*input->args);
-                        for (size_t j = i; j < argLen; j++) {
-                            if (input->args[j+1] != NULL && input->args[j+2] != NULL) {
-                                input->args[j+1] = input->args[j+2];
-                                input->args[strlen(*input->args)] = NULL;
-                            }
+void checkRedirect(struct Input *input) {
+    for (size_t i = 0; input->args[i] != NULL; i++) {
+        /* Check that the meta-character is not in the middle of the command, with no whitespace */
+        for(size_t k = 0; k < strlen(input->args[i]); k++) {
+            if (input->args[i][k] == '>') {
+                if (k !=0 && k != strlen(input->args[i])-1) {
+                    size_t length = strlen(input->args[i]);
+                    char currToken[length];
+                    strcpy(currToken, input->args[i]);
+                    char *newcmd = strtok(input->args[i], ">");
+                    input->args[i] = newcmd;
+                    newcmd = strtok(NULL, ">");
+                    strcpy(input->file, newcmd);
+                    input->willRedirect = 1;
+                    size_t argLen = strlen(*input->args);
+                    for (size_t j = i; j < argLen; j++) {
+                        if (input->args[j+1] != NULL && input->args[j+2] != NULL) {
+                            input->args[j+1] = input->args[j+2];
+                            input->args[strlen(*input->args)-1] = NULL;
                         }
                     }
                 }
             }
+        }
 
-            size_t argsLen = strlen(*input->args);
-            size_t tokenLen = strlen(input->args[i]);
+        size_t argsLen = strlen(*input->args);
+        size_t tokenLen = strlen(input->args[i]);
 
-            /* meta-character is at the beginning of the token */
-            if (input->args[i][0] == '>') {
-                    if (input->args[i][1] != '\0') {
-                            /* File name does not have a whitespace next to it */
-                            char *name = input->args[i] + 1;
-                            char fileName[strlen(name) + 1];
-                            strcpy(fileName, name);
-                            strcpy(input->file, fileName);
-                            /* Shift the char array to remove the file name argument */
-                            for (size_t j = i; j < argsLen; j++) {
-                                    input->args[j] = input->args[j+1];
-                                    input->args[strlen(*input->args)] = NULL;
+        /* meta-character is at the beginning of the token */
+        if (input->args[i][0] == '>') {
+                if (input->args[i][1] != '\0') {
+                        /* File name does not have a whitespace next to it */
+                        char *name = input->args[i] + 1;
+                        char fileName[strlen(name) + 1];
+                        strcpy(fileName, name);
+                        strcpy(input->file, fileName);
+                        /* Shift the char array to remove the file name argument */
+                        for (size_t j = i; j < argsLen; j++) {
+                            if (input->args[j+1] != NULL) {
+                                input->args[j] = input->args[j+1];
+                                input->args[strlen(*input->args)-1] = NULL;
+                            } else {
+                                input->args[j] = NULL;
+                                input->args[strlen(*input->args)-1] = NULL;
                             }
-                    } else if (input->args[i][1] == '\0') {
-                            /* File name has a whitespace next to it */
-                            strcpy(input->file, input->args[i+1]);
-                            for (size_t j = i; j < argsLen; j++) {
+                        }
+                } else if (input->args[i][1] == '\0') {
+                        /* File name has a whitespace next to it */
+                        strcpy(input->file, input->args[i+1]);
+                        for (size_t j = i; j < argsLen; j++) {
+                            if (input->args[j+2] != NULL) {
                                 input->args[j] = input->args[j+2];
-                                input->args[strlen(*input->args)] = NULL;
+                                input->args[strlen(*input->args)-1] = NULL;
+                            } else {
+                                input->args[j] = NULL;
+                                input->args[strlen(*input->args)-1] = NULL;
                             }
-                    }
-                    willRedirect = 1;
-            } else if (input->args[i][tokenLen-1] == '>') {
-                    strcpy(input->file, input->args[i+1]); // File name should be the next argument
-                    input->args[i][tokenLen-1] = '\0'; // Removes the meta-character from the argument
-                    for (size_t j = i; j < argsLen; j++) {
-                            input->args[j+1] = input->args[j+2];
-                            input->args[strlen(*input->args)] = NULL;
-                    }
-                    willRedirect = 1;
-            }
-  }
-  return willRedirect;
+                        }
+                }
+                input->willRedirect = 1;
+        } else if (input->args[i][tokenLen-1] == '>') { // meta-character is at end of token
+                strcpy(input->file, input->args[i+1]); // File name should be the next argument
+                input->args[i][tokenLen-1] = '\0'; // Removes the meta-character from the argument
+                for (size_t j = i; j < argsLen; j++) {
+                        input->args[j+1] = input->args[j+2];
+                        input->args[strlen(*input->args)-1] = NULL;
+                }
+                input->willRedirect = 1;
+        }
+    }
 };
 
 int main(void)
 {
-    struct Input input;
     struct Input piping[CMDLINE_MAX];
-    char tempPipe[CMDLINE_MAX];
+    char tempCmd[CMDLINE_MAX];
     int commandCount = 0;
     while (1) {
 
@@ -144,60 +146,60 @@ int main(void)
         fflush(stdout);
 
         /* Get command line */
-        fgets(tempPipe, CMDLINE_MAX, stdin);
+        fgets(tempCmd, CMDLINE_MAX, stdin);
 
         /* Print command line if stdin is not provided by terminal */
         if (!isatty(STDIN_FILENO)) {
-            printf("%s", tempPipe);
+            printf("%s", tempCmd);
             fflush(stdout);
         }
 
         /* Remove trailing newline from command line */
-        nl = strchr(tempPipe, '\n');
+        nl = strchr(tempCmd, '\n');
         if (nl)
             *nl = '\0';
 
         /* Builtin command */
-        if (!strcmp(tempPipe, "exit")) {
+        if (!strcmp(tempCmd, "exit")) {
             fprintf(stderr, "Bye...\n");
             break;
         }
+
         char message[CMDLINE_MAX];
-        strcpy(message, tempPipe);
-        printf("message: %s \n", message);
+        strcpy(message, tempCmd);
 
+        /* Send the raw input to be parsed for piping */
+        parsePipe(piping, tempCmd, &commandCount);
+        printf("[DEV]: commandCount: %d \n",commandCount);
 
-        /* Send the input to be parsed for arguments */
-        parsePipe(piping, tempPipe, &commandCount);
-
-        printf("commandCount: %d \n",commandCount);
         /* Regular command */
         for(int i = 0; i < commandCount; i++){
             parseInput(&piping[i]);
-            int willRedirect = checkRedirect(&piping[i]);
+            checkRedirect(&piping[i]);
 
             /* Remove this later, for debugging */
-            printf("[DEV]: list of arguments: %s \n", piping[i].cmd);
+            printf("[DEV]: current command: %s \n", piping[i].cmd);
+
             pid = fork();
 
             if (pid == 0) {
                 // Child
                 int fd;
-                if (willRedirect == 1) {
-                    fd = open(input.file, O_WRONLY | O_CREAT, 0644);
+                if (piping[i].willRedirect == 1) {
+                    fd = open(piping[i].file, O_WRONLY | O_CREAT, 0644);
                     dup2(fd, STDOUT_FILENO);
                     close(fd);
                 }
+
                 retval = execvp(piping[i].cmd, piping[i].args);
                 perror("execvp");
-
                 exit(1);
             } else if (pid > 0) {
                 // Parent
 
-                // Change directory
-                if (strcmp(input.cmd, "cd") == 0) {
-                    chdir(*input.args);
+                /* Change directory commands */
+                if (strcmp(piping[i].cmd, "cd") == 0) {
+                    chdir(*piping[i].args);
                 }
 
                 int status;
@@ -208,7 +210,6 @@ int main(void)
                 perror("fork");
                 exit(1);
             }
-            //   fprintf(stdout, "Return status value for '%s': %d\n", input.cmd, retval);
 
         }
         commandCount = 0;
